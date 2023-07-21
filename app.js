d01c59f9b39a44eee00beeff35fecd750e35cf1a
Daniel Jaburek <64271303+danieljaburek@users.noreply.github.com>
2023-07-22 00:27:38 +0200

    feat: export screenshot as pdf and png

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const opcua = require("./javascript/opcua");
 const config = require("./config/config.json");
+const puppeteer = require('puppeteer');
 
 (async () => {
   try {
@@ -52,6 +53,10 @@ const config = require("./config/config.json");
       console.log("Done");
       process.exit(0);
     });
+
+
+    // storing screenshot and pdf
+    await screenShotWebsite();
   } catch (err) {
     console.log(chalk.bgRed.white("Error" + err.message));
     console.log(err);
@@ -74,4 +79,38 @@ function runAtSpecificTimeOfDay(hour, minutes, func)
     // run every 24 hours from now on
     setInterval(func, twentyFourHours);
   }, eta_ms);
+}
+
+async function screenShotWebsite() {
+  const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Replace 'https://example.com' with the URL of the website you want to take a screenshot of
+    const websiteURL = 'http://localhost:3700/';
+    await page.goto(websiteURL, { waitUntil: 'networkidle2' });
+
+    // Customize screenshot options
+    const screenshotOptions = {
+      path: 'screenshot.png', // Output path for the screenshot image
+      fullPage: true, // Set to true to capture the full page
+      // You can also set width and height options to capture a specific viewport size:
+      // width: 1200,
+      // height: 800,
+    };
+
+    // Take a screenshot
+    await page.screenshot(screenshotOptions);
+
+    // Customize PDF options
+    const pdfOptions = {
+      path: 'website_screenshot.pdf', // Output path for the PDF
+      format: 'A4', // Paper format: 'A4', 'Letter', etc.
+      landscape: true, // Set to true for landscape orientation
+    };
+
+    // Generate the PDF from the screenshot image
+    await page.pdf(pdfOptions);
+
+    await browser.close();
+    console.log('Screenshot taken and PDF generated successfully.');
 }
